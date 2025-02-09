1921d59a0aae1562f3e2d7291a6efde1b3a693f4
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-02-09 21:48:54 +0530

    remove get route

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,6 @@ import express from "express";
 import { config } from "./config";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message:
-      "Welcome to the Ebook Store API! Explore our collection of ebooks and start your reading journey today.",
-  });
-});
-
 const startServer = async () => {
   const port = config.get("port");
   app.listen(port, () => {
