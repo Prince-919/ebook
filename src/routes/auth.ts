d823e16dff34bbe04c446868fa1a27512a19ba5c
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-04-05 12:49:23 +0530

    create validate middleware using zod library

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,19 +1,12 @@
 import { Router } from "express";
 import { AuthCtrl } from "@/controllers";
+import { emailValidationSchema, validate } from "@/middlewares/validator";
 
 const authRouter = Router();
 
 authRouter.post(
   "/generate-link",
-  (req, res, next) => {
-    const { email } = req.body;
-    const regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
-    if (!regex.test(email)) {
-      res.status(422).json({ error: "Invalid email!" });
-      return;
-    }
-    next();
-  },
+  validate(emailValidationSchema),
   AuthCtrl.generateAuthLink
 );
 
