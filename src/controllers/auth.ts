8b43cee1bc42554b20201ca55b611114398002f1
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-04-05 14:40:13 +0530

    store verification token in the database

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,10 +1,21 @@
 import { RequestHandler } from "express";
 import crypto from "crypto";
+import { UserModel, VerificationToken } from "@/models";
 
 class AuthCtrl {
   static generateAuthLink: RequestHandler = async (req, res) => {
     const { email } = req.body;
-    const token = crypto.randomBytes(36).toString();
+    let user = await UserModel.findOne({ email });
+    if (!user) {
+      user = await UserModel.create({ email });
+    }
+    const randomToken = crypto.randomBytes(36).toString("hex");
+    console.log(randomToken);
+
+    await VerificationToken.create<{ userId: string }>({
+      userId: user._id.toString(),
+      token: randomToken,
+    });
     res.json({ ok: true });
   };
 }
