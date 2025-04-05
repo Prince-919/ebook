7b25d2a3da84dc8d8df81ef8b937c3ff40b7ec1a
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-04-05 12:57:07 +0530

    generate random token using crypto

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,10 @@
 import { RequestHandler } from "express";
+import crypto from "crypto";
 
 class AuthCtrl {
   static generateAuthLink: RequestHandler = async (req, res) => {
+    const { email } = req.body;
+    const token = crypto.randomBytes(36).toString();
     res.json({ ok: true });
   };
 }
